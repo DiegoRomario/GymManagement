5770741880d10121b89efcff8d50add0024107c6
Diego Romário <diego.romario@outlook.com>
2024-05-13 15:53:43 -0300

    ddd approach

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -1,8 +1,10 @@
 using GymManagement.Application.Subscriptions.Commands.CreateSubscription;
+using GymManagement.Application.Subscriptions.Commands.DeleteSubscription;
 using GymManagement.Application.Subscriptions.Queries.GetSubscription;
 using GymManagement.Contracts.Subscriptions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using DomainSubscriptionType = GymManagement.Domain.Subscriptions.SubscriptionType;
 
 namespace GymManagement.Api.Controllers;
 
@@ -20,14 +22,28 @@ public class SubscriptionsController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateSubscription(CreateSubscriptionRequest request)
     {
-        var command = new CreateSubscriptionCommand(
+        if (!DomainSubscriptionType.TryFromName(
             request.SubscriptionType.ToString(),
+            out var subscriptionType))
+        {
+            return Problem(
+                statusCode: StatusCodes.Status400BadRequest,
+                detail: "Invalid subscription type");
+        }
+
+        var command = new CreateSubscriptionCommand(
+            subscriptionType,
             request.AdminId);
 
         var createSubscriptionResult = await _mediator.Send(command);
 
         return createSubscriptionResult.MatchFirst(
-            subscription => Ok(new SubscriptionResponse(subscription.Id, request.SubscriptionType)),
+            subscription => CreatedAtAction(
+                nameof(GetSubscription),
+                new { subscriptionId = subscription.Id },
+                new SubscriptionResponse(
+                    subscription.Id,
+                    ToDto(subscription.SubscriptionType))),
             error => Problem());
     }
 
@@ -41,7 +57,30 @@ public class SubscriptionsController : ControllerBase
         return getSubscriptionsResult.MatchFirst(
             subscription => Ok(new SubscriptionResponse(
                 subscription.Id,
-                Enum.Parse<SubscriptionType>(subscription.SubscriptionType))),
+                ToDto(subscription.SubscriptionType))),
             error => Problem());
+    }
+
+    [HttpDelete("{subscriptionId:guid}")]
+    public async Task<IActionResult> DeleteSubscription(Guid subscriptionId)
+    {
+        var command = new DeleteSubscriptionCommand(subscriptionId);
+
+        var createSubscriptionResult = await _mediator.Send(command);
+
+        return createSubscriptionResult.Match<IActionResult>(
+            _ => NoContent(),
+            _ => Problem());
+    }
+
+    private static SubscriptionType ToDto(DomainSubscriptionType subscriptionType)
+    {
+        return subscriptionType.Name switch
+        {
+            nameof(DomainSubscriptionType.Free) => SubscriptionType.Free,
+            nameof(DomainSubscriptionType.Starter) => SubscriptionType.Starter,
+            nameof(DomainSubscriptionType.Pro) => SubscriptionType.Pro,
+            _ => throw new InvalidOperationException(),
+        };
     }
 }
--- src/GymManagement.Infrastructure/Subscriptions/Persistence/SubscriptionsRepository.cs
+++ src/GymManagement.Infrastructure/Subscriptions/Persistence/SubscriptionsRepository.cs
@@ -1,6 +1,7 @@
 using GymManagement.Application.Common.Interfaces;
 using GymManagement.Domain.Subscriptions;
 using GymManagement.Infrastructure.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace GymManagement.Infrastructure.Subscriptions.Persistence;
 
@@ -15,11 +16,44 @@ public class SubscriptionsRepository : ISubscriptionsRepository
 
     public async Task AddSubscriptionAsync(Subscription subscription)
     {
-        _dbContext.Subscriptions.Add(subscription);
+        await _dbContext.Subscriptions.AddAsync(subscription);
     }
 
-    public async Task<Subscription?> GetByIdAsync(Guid subscriptionId)
+    public async Task<bool> ExistsAsync(Guid id)
     {
-        return await _dbContext.Subscriptions.FindAsync(subscriptionId);
+        return await _dbContext.Subscriptions
+            .AsNoTracking()
+            .AnyAsync(subscription => subscription.Id == id);
+    }
+
+    public async Task<Subscription?> GetByAdminIdAsync(Guid adminId)
+    {
+        return await _dbContext.Subscriptions
+            .AsNoTracking()
+            .FirstOrDefaultAsync(subscription => subscription.AdminId == adminId);
+    }
+
+    public async Task<Subscription?> GetByIdAsync(Guid id)
+    {
+        return await _dbContext.Subscriptions.FirstOrDefaultAsync(subscription => subscription.Id == id);
+    }
+
+    public async Task<List<Subscription>> ListAsync()
+    {
+        return await _dbContext.Subscriptions.ToListAsync();
+    }
+
+    public Task RemoveSubscriptionAsync(Subscription subscription)
+    {
+        _dbContext.Remove(subscription);
+
+        return Task.CompletedTask;
+    }
+
+    public Task UpdateAsync(Subscription subscription)
+    {
+        _dbContext.Update(subscription);
+
+        return Task.CompletedTask;
     }
 }
