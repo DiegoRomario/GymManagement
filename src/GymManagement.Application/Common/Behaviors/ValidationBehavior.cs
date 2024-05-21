179c42f1fb2e8e4a1260152d49212aafc5fd88cb
Diego Romário <diego.romario@outlook.com>
2024-05-20 21:06:24 -0300

    eventual consistency

--- src/GymManagement.Application/Common/Behaviors/ValidationBehavior.cs
+++ src/GymManagement.Application/Common/Behaviors/ValidationBehavior.cs
@@ -4,18 +4,23 @@ using MediatR;
 
 namespace GymManagement.Application.Common.Behaviors;
 
-public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest> validator)
+public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null)
     : IPipelineBehavior<TRequest, TResponse>
         where TRequest : IRequest<TResponse>
         where TResponse : IErrorOr
 {
-    private readonly IValidator<TRequest> _validator = validator;
+    private readonly IValidator<TRequest>? _validator = validator;
 
     public async Task<TResponse> Handle(
         TRequest request,
         RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
+        if (_validator is null)
+        {
+            return await next();
+        }
+
         var validationResult = await _validator.ValidateAsync(request, cancellationToken);
 
         if (validationResult.IsValid)
